fa9ff074cd12f5ab3b34b1878a4a1c6ca32ac144
Ilha Byrne <ilhabyrne@gmail.com>
2025-09-09 13:53:04 +0200

    terra info

---
+++ CBCS_coding_terra.R
@@ -0,0 +1,12 @@
+# Byrne, 2025
+# Spatial data - Terra
+# https://jflowernet.github.io/intro-terra
+
+# Load libraries ----
+library(terra)
+
+# Notes: ----
+# Raster data = grid
+# each grid cell has a value
+# ggplot tmap better for pub level plots
+# need to ensure projection is the same across all datasets & analyses
